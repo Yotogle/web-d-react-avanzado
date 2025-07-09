a7b0cfc7ef607112664d7b75f6ae2a9dfe391c45
Yotogle <jjtg630@gmail.com>
2025-07-08 20:32:18 -0600

    componente BasicForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { BasicForm } from './components/BasicForm'
 export const App = () => {
   return (
     <div>
       <h1>React avanzado</h1>
+      <BasicForm />
     </div>
   )
 }
---
+++ src/components/BasicForm.jsx
@@ -0,0 +1,17 @@
+import { useForm } from 'react-hook-form'
+
+export const BasicForm = () => {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type='text' {...register('username')} placeholder='Usuario' />
+      <input type='password' {...register('password')} placeholder='Usuario' />
+      <button type='submit'>Enviar</button>
+    </form>
+  )
+}
