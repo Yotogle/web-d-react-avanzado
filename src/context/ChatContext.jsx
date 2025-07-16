041aebd226494d2d5654620decd725abf0995852
Yotogle <jjtg630@gmail.com>
2025-07-16 00:37:10 -0600

    funcionalidad del chat finalizada

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { createContext, useReducer } from 'react'
 
-export const chatContext = () => {
+// 1. Crear el contexto global
+export const ChatContext = createContext()
+
+const initialState = {
+  messages: []
+}
+
+const chatReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      console.log('agregando mensaje...')
+      console.log(state)
+      return { ...state, messages: [...state.messages, action.payload] }
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload }
+    default:
+      return state
+  }
+}
+
+// 2. Provider
+
+export const ChatProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(chatReducer, initialState)
+
   return (
-    <div>chatContext</div>
+    <ChatContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
   )
 }
--- src/hooks/useOllama.js
+++ src/hooks/useOllama.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import axios from 'axios'
 
 export const useOllama = () => {
-  return (
-    <div>useOllama</div>
-  )
+  const sendMessage = async (userPrompt) => {
+    try {
+      const res = await axios.post('http://localhost:11434/api/generate', {
+        model: 'deepseek-r1:1.5b',
+        prompt: userPrompt,
+        stream: false
+      })
+      return res
+      // Dispatch para guardar el mensaje del usuario
+    } catch (error) {
+      console.error('error: ', error)
+    }
+  }
+
+  return { sendMessage }
 }
