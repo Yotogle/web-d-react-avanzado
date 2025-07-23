86058cef69e8e366eb608353adb45ecff25ea4f7
Yotogle <jjtg630@gmail.com>
2025-07-23 01:59:27 -0600

    rutas en server y post de backend

--- express/src/app.js
+++ express/src/app.js
@@ -1,12 +1,104 @@
+require('dotenv').config()
+console.log(process.env.PORT)
+console.log(process.env.NOMBRE)
+
 // 1 importamos modulo express
 const express = require('express')
+const { infoPeliculas } = require('./peliculas')
 // 2 creamos una aplicacion express
 const app = express()
 // 3 definimos el puerto que va a escuchar el servidor
 const PORT = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo')
+  res.send('Bienvenido a nuestra plataforma de peliculas')
+})
+app.get('/api/peliculas', (req, res) => {
+  res.send(infoPeliculas)
+})
+app.get('/api/peliculas/accion', (req, res) => {
+  res.send(infoPeliculas.accion)
+})
+app.get('/api/peliculas/comedia', (req, res) => {
+  res.send(infoPeliculas.comedia)
+})
+app.get('/api/peliculas/drama', (req, res) => {
+  res.send(infoPeliculas.drama)
+})
+app.get('/api/peliculas/cienciaFiccion', (req, res) => {
+  res.send(infoPeliculas.cienciaFiccion)
+})
+
+app.get('/api/peliculas/:titulo', (req, res) => {
+  const tituloBuscado = req.params.titulo
+  const todoslosGeneros = Object.values(infoPeliculas)
+  const todasLasPeliculas = todoslosGeneros.flat()
+  const resultados = todasLasPeliculas.filter(pelicula => pelicula.titulo.toLowerCase() === tituloBuscado.toLowerCase())
+  if (resultados.length > 0) {
+    res.send(resultados)
+  } else {
+    res.status(404).send({ mensaje: `No se encontraron peliculas con el nombre de ${tituloBuscado}` })
+  }
+})
+
+app.get('/api/peliculas/accion/:titulo', (req, res) => {
+  const titulo = req.params.titulo
+  const resultados = infoPeliculas.accion.filter(pelicula => pelicula.titulo === titulo)
+
+  if (resultados.length === 0) {
+    return res.status(400).send(`No se encontraron resultados para ${titulo}`)
+  }
+  res.send(resultados)
+})
+
+app.get('/api/peliculas/year/:year', (req, res) => {
+  const getAllMovies = () => {
+    const todoslosGeneros = Object.values(infoPeliculas)
+    return todoslosGeneros.flat()
+  }
+  const añoBuscado = req.params.year
+  const añoNumerico = Number(añoBuscado)
+  const todasLasPeliculas = getAllMovies()
+  const resultados = todasLasPeliculas.filter(pelicula => pelicula.year === añoNumerico)
+  if (resultados.length === 0) {
+    return res.status(400).send(`No se encontraron peliculas del año ${añoBuscado}`)
+  }
+  res.send(resultados)
+})
+app.get('/api/peliculas/accion/titulo/:titulo/:year', (req, res) => {
+  /* const titulo = req.params.titulo
+  const year = req.params.year */
+  const { titulo, year } = req.params
+  const resultados = infoPeliculas.accion.filter(pelicula => pelicula.titulo === titulo && pelicula.year === Number(year))
+
+  if (resultados.length === 0) {
+    return res.status(400).send(`No se encontraron resultados para ${titulo} en el año ${year}`)
+  }
+
+  res.send(resultados)
+})
+
+app.get('/api/peliculas/comedia/:pais', (req, res) => {
+  const pais = req.params.pais
+  const resultados = infoPeliculas.comedia.filter(pelicula => pelicula.pais === pais)
+
+  if (req.query.ordenar === 'year') {
+    return res.send(resultados.sort((a, b) => b.year - a.year))
+  }
+
+  res.send(resultados)
+})
+
+// Metodo para subir informaccion al backend
+app.use(express.json())
+app.post('/api/peliculas', (req, res) => {
+  const nuevaPelicula = req.body
+
+  console.log(nuevaPelicula)
+  res.status(201).send({
+    mensaje: 'La película se recibió con éxito',
+    datos: nuevaPelicula
+  })
 })
 // 4
 app.listen(PORT, () => {
---
+++ express/src/peliculas.js
@@ -0,0 +1,130 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'Misión Imposible: Nación Secreta',
+      year: 2015,
+      pais: 'USA'
+    },
+    {
+      id: 2,
+      titulo: 'Mad Max: Furia en la Carretera',
+      year: 2015,
+      pais: 'Australia'
+    },
+    {
+      id: 3,
+      titulo: 'John Wick: Capítulo 3 - Parabellum',
+      year: 2019,
+      pais: 'USA'
+    },
+    {
+      id: 4,
+      titulo: 'Venganza (Taken)',
+      year: 2008,
+      pais: 'Francia'
+    },
+    {
+      id: 5,
+      titulo: 'Duro de Matar',
+      year: 1988,
+      pais: 'USA'
+    },
+    {
+      id: 6,
+      titulo: 'El Caballero Oscuro',
+      year: 2008,
+      pais: 'USA'
+    }
+  ],
+  comedia: [
+    {
+      id: 7,
+      titulo: '¿Qué Pasó Ayer?',
+      year: 2009,
+      pais: 'USA'
+    },
+    {
+      id: 8,
+      titulo: 'Superbad',
+      year: 2007,
+      pais: 'USA'
+    },
+    {
+      id: 9,
+      titulo: 'Amelie',
+      year: 2001,
+      pais: 'Francia'
+    },
+    {
+      id: 10,
+      titulo: 'Una Noche en la Ópera',
+      year: 1935,
+      pais: 'USA'
+    },
+    {
+      id: 11,
+      titulo: 'La Vida de Brian',
+      year: 1979,
+      pais: 'Reino Unido'
+    }
+  ],
+  drama: [
+    {
+      id: 12,
+      titulo: 'Sueños de Fuga',
+      year: 1994,
+      pais: 'USA'
+    },
+    {
+      id: 13,
+      titulo: 'El Padrino',
+      year: 1972,
+      pais: 'USA'
+    },
+    {
+      id: 14,
+      titulo: 'La Lista de Schindler',
+      year: 1993,
+      pais: 'USA'
+    },
+    {
+      id: 15,
+      titulo: 'Forrest Gump',
+      year: 1994,
+      pais: 'USA'
+    }
+  ],
+  cienciaFiccion: [
+    {
+      id: 16,
+      titulo: 'Blade Runner 2049',
+      year: 2017,
+      pais: 'USA'
+    },
+    {
+      id: 17,
+      titulo: 'Origen',
+      year: 2010,
+      pais: 'USA'
+    },
+    {
+      id: 18,
+      titulo: 'Matrix',
+      year: 1999,
+      pais: 'USA'
+    },
+    {
+      id: 19,
+      titulo: 'Interestelar',
+      year: 2014,
+      pais: 'USA'
+    }
+
+  ]
+}
+
+module.exports.infoPeliculas = infoPeliculas
+/*
+const _infoPeliculas = infoPeliculas
+export { _infoPeliculas as infoPeliculas } */
